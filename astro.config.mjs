124be588541c26d9efc674cbc4725da097703436
afterglowhub <liumoon.com@gmail.com>
2026-02-08 12:53:06 +0900

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://blog.afterglowhub.com',  // ← ここを変更
 	integrations: [mdx(), sitemap()],
 });
